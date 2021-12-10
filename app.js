23e90cd3259ed2f8fcda32fc1c0c90ae7388001e
Dhruv <samantdhruv@gmail.com>
2021-12-10 19:06:49 +0530

    adding list funtion with hard inputted value

---
+++ app.js
@@ -0,0 +1,38 @@
+// Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+// Event Listeners
+todoButton.addEventListener("click", addToDo);
+
+//Functions
+
+function addToDo(event) {
+  event.preventDefault();
+
+  // creating div element
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+
+  // creating li element
+  const newTodo = document.createElement("li");
+  newTodo.innerText = "Hello world";
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+
+  // creating button (add)
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = `<i class="fas fa-check"></li>`;
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
+
+  // creating button (trash)
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = `<i class="fas fa-trash"></li>`;
+  trashButton.classList.add("complete-btn");
+  todoDiv.appendChild(trashButton);
+
+  // Append to list
+  todoList.appendChild(todoDiv)
+}
