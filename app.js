6a4b7e22a9f114011cdaf6ec0ee715a9df64cd98
Dhruv <samantdhruv@gmail.com>
2021-12-10 20:44:40 +0530

    removing todos from the local storage

--- app.js
+++ app.js
@@ -53,6 +53,7 @@ function deleteCheck(e) {
   if (item.classList[0] === "trash-btn") {
     const todo = item.parentElement;
     todo.classList.add("fall");
+    removeLocalTodos(todo);
     todo.addEventListener("transitionend", function () {
       todo.remove();
     });
@@ -140,4 +141,18 @@ function getTodos() {
     // Append to list
     todoList.appendChild(todoDiv);
   });
+}
+
+function removeLocalTodos(todo) {
+  // check -- already in list?
+  let todos;
+
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  const todoIndex = todo.children[0].innerText;
+  todos.splice(todos.indexOf(todoIndex), 1);
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
